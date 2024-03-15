9ca6f52e84ba1d1a9d951e5db354a11cdb1593cf
davidebernasconi13 <davideb.bernasconi@gmail.com>
2024-03-15 17:04:54 +0100

    algoritmo iniziale da perfezionare!!!

--- asset/js/main.js
+++ asset/js/main.js
@@ -2,14 +2,31 @@ let btnActived = document.getElementById('btn');
 const grid = document.querySelector('.grid');
 
 
-btnActived.addEventListener('click', function () {
-    // faccio apparire il contenitore della griglia
-    grid.classList.toggle('d-none');
-})
 
-function generateCell() {
+
+function generateCell(contatore) {
     // aggiungere l'elemento cell
     let cell = document.createElement('div');
+    cell.setAttribute('class', 'cell');
+    cell.innerHTML = contatore + 1;
     return cell
 }
 console.log(generateCell());
+
+btnActived.addEventListener('click', function () {
+    // rimuovo eventuali griglie generate
+    // faccio apparire il contenitore della griglia
+    grid.classList.toggle('d-none');
+
+    //cliclo i numeri da 1 a 100 e li metto dentro le celle
+    for (let i = 0; i < 100; i++) {
+        grid.appendChild(generateCell(i));
+    }
+
+})
+
+let cellClicked = document.querySelector('.cell');
+cellClicked.addEventListener('click', function () {
+    classList.add('clicked');
+})
+
